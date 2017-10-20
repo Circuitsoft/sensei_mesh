51f1c92ea36f30d95a5f578ccf5aefb3dc84b763
Pete Schwamb <pete@schwamb.net>
2017-10-20 13:39:32 -0500

    Updated pin definitions to match prototype board design

--- bsp/lesson_tracker_nrf52.h
+++ bsp/lesson_tracker_nrf52.h
@@ -8,13 +8,36 @@ extern "C" {
 #define MMA8541
 #define JOSTLE_DETECT
 
-#define LEDS_NUMBER    0
-#define BUTTONS_NUMBER 0
+// LEDs definitions for SENSEI_AREA_H
+#define LEDS_NUMBER    1
 
-#define RX_PIN_NUMBER  0
-#define TX_PIN_NUMBER  1
-//#define CTS_PIN_NUMBER 0xff
-//#define RTS_PIN_NUMBER 0xff
+#define LED_START      8
+#define LED_1          8
+#define LED_STOP       8
+
+#define LEDS_ACTIVE_STATE 0
+
+#define LEDS_INV_MASK  LEDS_MASK
+
+#define LEDS_LIST { LED_1 }
+
+#define BSP_LED_0      LED_1
+
+#define BUTTONS_NUMBER 1
+
+#define BUTTON_START   21
+#define BUTTON_1       21
+#define BUTTON_STOP    21
+#define BUTTON_PULL    NRF_GPIO_PIN_PULLUP
+
+#define BUTTONS_ACTIVE_STATE 0
+
+#define BUTTONS_LIST { BUTTON_1 }
+
+#define BSP_BUTTON_0   BUTTON_1
+
+#define RX_PIN_NUMBER  3
+#define TX_PIN_NUMBER  2
 #define HWFC           false
 
 #ifdef __cplusplus
